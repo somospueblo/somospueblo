26021b2bebdd303b727f2d677e425198e810017a
somospueblo <74834427+somospueblo@users.noreply.github.com>
2021-02-23 10:29:32 -1000

    Update encript.js

--- encript.js
+++ encript.js
@@ -7,12 +7,12 @@ const inventario = [
   { nombre:"Rafael Arcangel Quispe Flores" ,ci:3412812,num:"# 1002",cargo:"Presidente" ,dep:"La Paz" ,mun:"Nuestra Señora de La Paz" ,dist:"2"},
   { nombre:"Josue Viktor Morales Choque" ,ci:6957485,num:"# 1001",cargo:"Jefe Circunscripción" ,dep:"La Paz" ,mun:"La Paz" ,dist:"37"},
   { nombre:"Rodrigo Salazar" ,ci:8468662,num:"# 1666",cargo:"aux" ,dep:"La Paz" ,mun:"La Paz" ,dist:"7"},
-  { nombre:"CARLOS HUMBERTO CALDERÓN ARGOTE" ,ci:6133193 LP,num:"# 1020",cargo:"STAFF/REDES" ,dep:"La Paz" ,mun:"La Paz" ,dist:"-"},
-{ nombre:"MARY ALEJANDRA LUCÍA VELASCO" ,ci:4938524 LP,num:"# 1021",cargo:"STAFF/REDES" ,dep:"La Paz" ,mun:"La Paz" ,dist:"-"},
-{ nombre:"CRISTIAN MERCADO LOPEZ" ,ci:9614211 SC,num:"# 1022",cargo:"STAFF/REDES" ,dep:"La Paz" ,mun:"La Paz" ,dist:"-"},
-{ nombre:"JHOJAN EMANUEL TERRAZAS FLORES" ,ci:9135350 LP,num:"# 1023",cargo:"STAFF/REDES" ,dep:"La Paz" ,mun:"La Paz" ,dist:"-"},
-{ nombre:"LUIS MIGUEL CARITA CUTILLI" ,ci:6728967 LP,num:"# 1024",cargo:"STAFF/REDES" ,dep:"La Paz" ,mun:"La Paz" ,dist:"-"},
-{ nombre:"MARIO ANTONIO USNAYO NINACHOQUE" ,ci:8448980 LP,num:"# 1025",cargo:"STAFF/REDES" ,dep:"La Paz" ,mun:"La Paz" ,dist:"-"},
+  { nombre:"CARLOS HUMBERTO CALDERÓN ARGOTE" ,ci:6133193,num:"# 1020",cargo:"STAFF/REDES" ,dep:"La Paz" ,mun:"La Paz" ,dist:"-"},
+{ nombre:"MARY ALEJANDRA LUCÍA VELASCO" ,ci:4938524,num:"# 1021",cargo:"STAFF/REDES" ,dep:"La Paz" ,mun:"La Paz" ,dist:"-"},
+{ nombre:"CRISTIAN MERCADO LOPEZ" ,ci:9614211,num:"# 1022",cargo:"STAFF/REDES" ,dep:"La Paz" ,mun:"La Paz" ,dist:"-"},
+{ nombre:"JHOJAN EMANUEL TERRAZAS FLORES" ,ci:9135350,num:"# 1023",cargo:"STAFF/REDES" ,dep:"La Paz" ,mun:"La Paz" ,dist:"-"},
+{ nombre:"LUIS MIGUEL CARITA CUTILLI" ,ci:6728967,num:"# 1024",cargo:"STAFF/REDES" ,dep:"La Paz" ,mun:"La Paz" ,dist:"-"},
+{ nombre:"MARIO ANTONIO USNAYO NINACHOQUE" ,ci:8448980,num:"# 1025",cargo:"STAFF/REDES" ,dep:"La Paz" ,mun:"La Paz" ,dist:"-"},
 
 ];
 var item = inventario.find(item => item.ci === parseInt(prodId, 10));
