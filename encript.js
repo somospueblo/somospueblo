33396b2c498d0ca148d65cd4bf8cd0cf96c72bcc
somospueblo <74834427+somospueblo@users.noreply.github.com>
2021-03-05 14:08:10 -1000

    Update encript.js

--- encript.js
+++ encript.js
@@ -3,16 +3,22 @@ var prodId = getParameterByName('prodId');
 console.log(prodId);  */
 const inventario = [
 
-  { nombre:"Victor Joel Huancollo Galindo" ,ci:11107990,num:"# 1000",cargo:"aux" ,dep:"La Paz" ,mun:"La Paz" ,dist:"7"},
-  { nombre:"Rafael Arcangel Quispe Flores" ,ci:3412812,num:"# 1002",cargo:"Presidente" ,dep:"La Paz" ,mun:"Nuestra Señora de La Paz" ,dist:"2"},
-  { nombre:"Josue Viktor Morales Choque" ,ci:6957485,num:"# 1001",cargo:"Jefe Circunscripción" ,dep:"La Paz" ,mun:"La Paz" ,dist:"37"},
-  { nombre:"Rodrigo Salazar" ,ci:8468662,num:"# 1666",cargo:"aux" ,dep:"La Paz" ,mun:"La Paz" ,dist:"7"},
-  { nombre:"CARLOS HUMBERTO CALDERÓN ARGOTE" ,ci:6133193,num:"# 1020",cargo:"STAFF/REDES" ,dep:"La Paz" ,mun:"La Paz" ,dist:"-"},
-{ nombre:"MARY ALEJANDRA LUCÍA VELASCO" ,ci:4938524,num:"# 1021",cargo:"STAFF/REDES" ,dep:"La Paz" ,mun:"La Paz" ,dist:"-"},
-{ nombre:"CRISTIAN MERCADO LOPEZ" ,ci:9614211,num:"# 1022",cargo:"STAFF/REDES" ,dep:"La Paz" ,mun:"La Paz" ,dist:"-"},
-{ nombre:"JHOJAN EMANUEL TERRAZAS FLORES" ,ci:9135350,num:"# 1023",cargo:"STAFF/REDES" ,dep:"La Paz" ,mun:"La Paz" ,dist:"-"},
-{ nombre:"LUIS MIGUEL CARITA CUTILLI" ,ci:6728967,num:"# 1024",cargo:"STAFF/REDES" ,dep:"La Paz" ,mun:"La Paz" ,dist:"-"},
-{ nombre:"MARIO ANTONIO USNAYO NINACHOQUE" ,ci:8448980,num:"# 1025",cargo:"STAFF/REDES" ,dep:"La Paz" ,mun:"La Paz" ,dist:"-"},
+  { nombre:"JOSUE VIKTOR MORALES CHOQUE" ,ci:6957485,num:"# 1001",cargo:"PRESIDENTE" ,dep:" " ,mun:"-" ,dist:"-"},
+{ nombre:"FERNANDO F.A. BOLLATI BUTRON" ,ci:3449966,num:"# 1002",cargo:"VICEPRESIDENTE ,dep:"INSTITUTO AMERICANO" ,mun:"-" ,dist:"-"},
+{ nombre:"YAZZEL YAMIL DUK ESCOBAR" ,ci:13608507,num:"# 1003",cargo:"AREA LEGAL ,dep:"ESC. PANAMA" ,mun:"-" ,dist:"-"},
+{ nombre:"VICTOR JOEL HUANCOLLO GALINDO" ,ci:11107990,num:"# 1004",cargo:"AREA INFORMATICA ,dep:"ESC. SERGIO SUAREZ" ,mun:"-" ,dist:"-"},
+{ nombre:"AIRTON SOTO PARDO" ,ci:8469891,num:"# 1005",cargo:"SECRETARIO GENERAL ,dep:"WALDO BALLIVIAN" ,mun:"-" ,dist:"-"},
+{ nombre:"CARLOS CALDERON ARGOTE" ,ci:6133193,num:"# 1006",cargo:"SECRETARIO DE COMUNICACIÓN ,dep:"COL. GERMAN BUSCH" ,mun:"-" ,dist:"-"},
+{ nombre:"ANDREA DAVALOS QUIROGA" ,ci:5943404,num:"# 1007",cargo:"SECRETARIA DE ACTAS ,dep:"COL. COPACABANA" ,mun:"-" ,dist:"-"},
+{ nombre:"BORIS ARISPE FERRUINO" ,ci:4894063,num:"# 1008",cargo:"ENCARGADO DE REDES ,dep:" " ,mun:"-" ,dist:"-"},
+{ nombre:"JORGE CALANCHA MAREÑO" ,ci:4899062,num:"# 1009",cargo:"RECURSOS HUMANOS ,dep:"COL. GREGORIO REYNOLDS" ,mun:"-" ,dist:"-"},
+{ nombre:"DANIELA BUENO FERNANDEZ" ,ci:4032096,num:"# 1010",cargo:"TESORERA ,dep:" ESC. 6 DE AGOSTO" ,mun:"-" ,dist:"-"},
+{ nombre:"FELIX ARIAS RUBIN DE CELIS" ,ci:8463219,num:"# 1011",cargo:"CULTURAS Y DEPORTES ,dep:" ESC. FRANCISCO DE MIRANDA" ,mun:"-" ,dist:"-"},
+{ nombre:"FERNANDO CARPIO" ,ci:4777364,num:"# 1012",cargo:"PLANIFICACION ,dep:"ESC. CHASQUIPAMPA" ,mun:"-" ,dist:"-"},
+{ nombre:"ALEXIS LOZA " ,ci:6159947,num:"# 1013",cargo:"DIRECCION PRODUCCION ,dep:"" ,mun:"-" ,dist:"-"},
+{ nombre:"BETTY TIVERO QUISPE" ,ci:4327421,num:"# 1014",cargo:"SECRETARIA DE GENERO ,dep:"COL. CANADA" ,mun:"-" ,dist:"-"},
+{ nombre:"EDMAR YANA VILLARROEL" ,ci:5960411,num:"# 1015",cargo:"LOGISTICA ,dep:"COL. CRISTO REY" ,mun:"-" ,dist:"-"},
+{ nombre:"-" ,ci:12345,num:"# 1016",cargo:"- ,dep:"La Paz" ,mun:"-" ,dist:"-"},
 
 ];
 var item = inventario.find(item => item.ci === parseInt(prodId, 10));
